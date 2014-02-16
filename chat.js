e8901ada0ca235c1bf8c1e817ffc3c2be38f794c
krzoz <osirk.ruiz@globallynx.com>
2014-02-15 18:59:51 -0600

    chat

--- chat.js
+++ chat.js
@@ -1,9 +1,12 @@
 var express = require("express");
 var dust = require("dustjs-linkedin");
 var cons = require("consolidate");
+var http = require("http"); // libreria interna
+var socketio = require("socket.io");
 
 var app = express();
-app.listen(8021);
+var servidor = http.createServer(app);
+servidor.listen(8021);
 
 //---- configuracion de carpetas estaticas ----
 app.use("/css", express.static(__dirname + "/css"));
@@ -15,3 +18,24 @@ app.set("views", __dirname + "/vistas");
 app.set("view engine", "dust");
 
 console.log("servidor web listo");
+
+
+app.get("/", function(req, res){
+	res.render("cliente");
+});
+
+//habilitamos el websocket en el servidor
+var io = socketio.listen(servidor);
+
+//la variable socket representa al usuario q se conecto
+io.sockets.on("connection", function(socket){
+	//el on significa cuando ocurra un evento
+	//el primer  argumento es el mnombre del evento
+	//el segundo argumento es la función q ejecutamos en consecuencia
+	socket.on("mensaje_al_servidor",function(datosCliente){
+		//representa a todos los usuarios conectados
+		//emit = produce un evento que puede escuchar el cliente o el servidor
+		io.socket.emit("mensaje_al_cliente", datosCliente);
+	});
+});
+
